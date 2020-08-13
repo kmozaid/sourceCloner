f176ddc83b7235b32872a57cc0dfdaab78a620ab
Mohemmad Zaid Khan <mohemmadzaid.khan@broadcom.com>
2020-08-13 20:24:13 +0530

    Use interface

--- service/oauth.go
+++ service/oauth.go
@@ -1,13 +1,47 @@
 package service
 
-import "github.com/mozaidk/sourceCloner/service/github"
+import (
+	"github.com/mozaidk/sourceCloner/config"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+)
 
-var gitHubOAuthService = github.AuthService{}
+type OAuthService interface {
+	authorizeURL() string
+	accessToken(code string) string
+}
+
+type GitHubOAuthService struct {
+	conf oauth2.Config
+}
+
+func (g GitHubOAuthService) authorizeURL() string {
+	return g.conf.AuthCodeURL("state")
+}
+
+func (g GitHubOAuthService) accessToken(code string) string {
+	token, err := g.conf.Exchange(context.Background(), code)
+	if err != nil {
+		return ""
+	}
+	return token.AccessToken
+}
+
+var gitHubOAuthService = GitHubOAuthService{
+	conf: oauth2.Config{
+		ClientID:     config.ServiceConf.ClientId,
+		ClientSecret: config.ServiceConf.ClientSecret,
+		Endpoint:     github.Endpoint,
+		RedirectURL:  config.ServiceConf.RedirectUri,
+		Scopes:       config.ServiceConf.Scopes,
+	},
+}
 
 func AuthorizeURL() string {
-	return gitHubOAuthService.AuthorizeURL()
+	return gitHubOAuthService.authorizeURL()
 }
 
 func AccessToken(code string) string {
-	return gitHubOAuthService.AccessToken(code)
+	return gitHubOAuthService.accessToken(code)
 }
